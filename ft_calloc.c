0f063bdeaf5496719ebc8c050bcea33ec056cdbc
Thayhabeck <thaynahabeck@gmail.com>
2022-05-03 20:42:36 -0300

    calloc protection

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,28 @@
 /*   By: thabeck- <thabeck-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:10:08 by thabeck-          #+#    #+#             */
-/*   Updated: 2022/04/21 19:45:11 by thabeck-         ###   ########.fr       */
+/*   Updated: 2022/05/04 01:10:58 by thabeck-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
+	size_t	count;
 
-	ptr = malloc(count * size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, count * size);
+	count = nmemb * size;
+	if (count)
+	{
+		if ((count / nmemb) != size)
+			return (NULL);
+		ptr = malloc(count);
+		if (ptr == NULL)
+			return (NULL);
+		ft_bzero(ptr, count);
+	}
+	else
+		ptr = malloc(0);
 	return (ptr);
 }
